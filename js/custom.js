4cba1d5f0f27fa613234c492dbe82abc2d6dab26
durga <durga@livsoft.co>
2020-09-22 18:31:23 +0530

    first commit

---
+++ js/custom.js
@@ -0,0 +1,51 @@
+/*click scroll down*/
+
+/*owl carousel*/
+$(document).ready(function () {
+    $('.demo1').owlCarousel({
+        loop: true,
+        margin: 0,
+        nav: false,
+        dots: true,
+        responsive: {
+            0: {
+                items: 1
+            },
+            600: {
+                items: 1
+            },
+            1000: {
+                items: 1
+            }
+        }
+    })
+});
+
+/*Sticky Header*/
+
+/*accordian*/
+$(document).ready(function () {
+
+    $(".toggle-accordion").on("click", function () {
+        var accordionId = $(this).attr("accordion-id"),
+            numPanelOpen = $(accordionId + ' .collapse.in').length;
+
+        $(this).toggleClass("active");
+
+        if (numPanelOpen == 0) {
+            openAllPanels(accordionId);
+        } else {
+            closeAllPanels(accordionId);
+        }
+    })
+
+    openAllPanels = function (aId) {
+        console.log("setAllPanelOpen");
+        $(aId + ' .panel-collapse:not(".in")').collapse('show');
+    }
+    closeAllPanels = function (aId) {
+        console.log("setAllPanelclose");
+        $(aId + ' .panel-collapse.in').collapse('hide');
+    }
+
+});
